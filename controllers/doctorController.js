9256f2d0bdc6fd811cfa1c098edaecdf6202dddc
Eya Garali <143196960+Eya-Hub@users.noreply.github.com>
2025-01-20 12:35:00 +0100

    add new files

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -8,7 +8,7 @@ const searchDoctorsByName = (req, res) => {
         return res.status(400).json({ message: 'Please enter at least 3 characters to search.' });
     }
 
-    const query = "SELECT name AS doctor_name FROM doctors WHERE name LIKE ?";
+    const query = "SELECT doctors.name,doctors.price,doctors.discount_price,doctors.description,doctors.session_duration,specialities.name as speciality, hospitals.Name FROM `doctors`,`specialities`,`hospitals`,`hospital_cnv` WHERE doctors.name LIKE ? AND specialities.id = doctors.speciality_id AND hospital_cnv.id_hospital = doctors.hospital_id AND hospitals.id_hospital = hospital_cnv.id_hospital;";
     const searchValue = `${name}%`;
 
     console.log('Executing query:', query); // Log the query
@@ -19,6 +19,7 @@ const searchDoctorsByName = (req, res) => {
             console.error('Error searching doctors:', err);
             return res.status(500).json({ message: 'Failed to search doctors.' });
         }
+        //console.log('Query results:', results);
         res.status(200).json(results); 
     });
 };
